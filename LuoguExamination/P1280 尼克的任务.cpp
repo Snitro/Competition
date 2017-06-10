28ee0bdeb081689596d408587728b307eb56eb2d
“SniffesTheRose” <1161052526@qq.com>
2017-06-10 19:47:10 +0800

    SCHOOL P1280

--- LuoguExamination/P1280 尼克的任务.cpp
+++ LuoguExamination/P1280 尼克的任务.cpp
@@ -10,7 +10,7 @@ struct Job{
     int end;
 }job[10005];
 int main(){
-   	freopen("in.txt","r",stdin);
+    freopen("in.txt","r",stdin);
     cin >> n >> k;
     for(int i = 1;i <= k;i++){
         scanf("%d%d",&job[i].begin,&job[i].need);
@@ -18,22 +18,22 @@ int main(){
     }
     F[n] = 0;
     for(int i = n;i >= 1;i--){
-    	if(job[k].begin == i){
-    		F[i] = n + 1;
-    		for(int j = k;job[j].begin == i;j--){
-				if(F[i] == 0){
-					F[i] = F[job[j].end] + job[j].need;
-				}
-				else
-					F[i] = min(F[i],F[job[j].end] + job[j].need);
-				k--;
-    		}
-    	}else
-    		F[i] = F[i + 1];
+        if(job[k].begin == i){
+            F[i] = n + 1;
+            for(int j = k;job[j].begin == i;j--){
+                if(F[i] == 0){
+                    F[i] = F[job[j].end] + job[j].need;
+                }
+                else
+                    F[i] = min(F[i],F[job[j].end] + job[j].need);
+                k--;
+            }
+        }else
+            F[i] = F[i + 1];
     }
     /*
     for(int i = 1;i <= n;i++)
-    	cout << F[i] << " ";
+        cout << F[i] << " ";
     */
     cout << n - F[1] ;
 }
