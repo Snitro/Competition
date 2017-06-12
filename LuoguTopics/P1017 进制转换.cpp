eb44c7b545234a795204a1e82aa5c101b3f814e8
“SniffesTheRose” <1161052526@qq.com>
2017-06-12 19:20:38 +0800

    SCHOOL 2017 6 12 P1017

---
+++ LuoguTopics/P1017 进制转换.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstdio>
+#include<stack>
+using namespace std;
+stack<int> ans;
+int n,j;
+char ji[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K'};
+/*
+由于默认向下取整
+7 / -2 = -4  ··· -1
+但是结果中不应出现负数
+7 / -2 = -3  ··· 1
+*/
+int main(){
+	freopen("in.txt","r",stdin);
+	cin >> n >> j;
+	cout << n << "=";
+	while(n != 0){
+		int x = n % j;
+		n = n / j;
+		if(x < 0){
+			x -= j;
+			n++;
+		}
+		ans.push(x);
+	}
+	while(!ans.empty()){
+		cout << ji[ans.top()];
+		ans.pop();
+	}
+	cout <<"(base" << j << ")";
+	return 0;
+}
