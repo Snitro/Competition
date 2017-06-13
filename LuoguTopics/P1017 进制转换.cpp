4bbb68f69390675ded93b8bdf1a116a05ef7c262
“SniffesTheRose” <1161052526@qq.com>
2017-06-13 09:05:23 +0800

    SCHOOL 2017 6 13 UpData

--- LuoguTopics/P1017 进制转换.cpp
+++ LuoguTopics/P1017 进制转换.cpp
@@ -1,22 +1,33 @@
 #include<iostream>
 #include<cstdio>
+#include<stack>
 using namespace std;
-int a,b;
-void gcd(int a,int b,int &d,int &x,int &y){
-	if(!b){
-		d = a;
-		x = 1;
-		y = 0;
-	}else{
-		gcd(b,a % b,d,y,x);
-		y -= x * (a / b);
-	}
-}
+stack<int> ans;
+int n,j;
+char ji[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K'};
+/*
+由于默认向0取整
+-3 / -2 = 1 ··· -1 
+但是结果中不应出现负数
+-3 / -2 = 2  ··· 1
+*/
 int main(){
 	freopen("in.txt","r",stdin);
-	cin >> a >> b;
-	int d,x,y;
-	gcd(a,b,d,x,y);
-	cout << (x + b) % b;
+	cin >> n >> j;
+	cout << n << "=";
+	while(n != 0){
+		int x = n % j;
+		n = n / j;
+		if(x < 0){
+			x -= j;
+			n++;
+		}
+		ans.push(x);
+	}
+	while(!ans.empty()){
+		cout << ji[ans.top()];
+		ans.pop();
+	}
+	cout <<"(base" << j << ")";
 	return 0;
 }
