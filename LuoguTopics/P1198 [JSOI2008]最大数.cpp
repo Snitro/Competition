22e6b9ce769ede311b85ceeafcb2e57f99704e83
物是人非 <1161052526@qq.com>
2017-09-06 21:42:28 +0800

    SCHOOL 2017 9 6 单调栈

---
+++ LuoguTopics/P1198 [JSOI2008]最大数.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+long long m, d;
+
+int top;
+
+int last;
+
+int len;
+
+int stack[200001];
+int datas[200001];
+
+int main() {
+	freopen("in.txt", "r", stdin);
+	
+	scanf("%d %d", &m, &d);
+	
+	char c[1];
+	int x; 
+	
+	while (m--) {
+		scanf("%s %d", c, &x);	
+		
+		if (c[0] == 'A') {
+			x = (x + last) % d;
+			
+			datas[++len] = x;
+			
+			while (top && datas[stack[top]] <= x)
+				top--;
+			
+			stack[++top] = len;
+		} else {
+			int y = lower_bound(stack + 1, stack + top + 1, len - x + 1) - stack;
+            printf("%d\n", last = datas[stack[y]]);
+		}
+	}
+	
+	return 0;
+}
