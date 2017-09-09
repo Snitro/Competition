e4692a3796869e6cfce058b6f6894da69b0c875d
物是人非 <1161052526@qq.com>
2017-09-10 07:00:17 +0800

    SCHOOL 2017 9 10 优化动态规划

--- LuoguTopics/P1681 最大正方形II.cpp
+++ LuoguTopics/P1681 最大正方形II.cpp
@@ -3,50 +3,33 @@
 
 using namespace std;
 
-int n, m;
-
-int datas[1505][1505];
-
 int map[1505][1505];
 
-int ans;
+int f[1505][1505][2];
 
-void solve() {
-	for (int y = 1; y <= n; y++)
-		for (int x = 1; x <= m; x++)
-		    if (map[x][y]) {
-		        map[x][y] = min(map[x - 1][y], map[x][y - 1]) + 1;
-		        
-		        if (map[x][y] >= 2 && !map[x - map[x][y] + 1][y - map[x][y] + 1])
-		            map[x][y]--;
-		        
-		        if (map[x][y] > ans)
-		            ans = map[x][y];
-		    }
-}
+int n, m, ans;
 
 int main() {
     freopen("in.txt", "r", stdin);
     
     scanf("%d%d", &n, &m);
     
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++) { 
-            scanf("%d", &datas[j][i]);
-            datas[j][i] ^= (i ^ j) & 1;
-            map[j][i] = datas[j][i];
-        } 
-    
-	solve();
+    for (int y = 1; y <= n; y++)
+        for (int x = 1; x <= m; x++)
+            scanf("%d", &map[x][y]);
             
     for (int y = 1; y <= n; y++)
-    	for (int x = 1; x <= m; x++)
-    		map[x][y] = !datas[x][y];
-    
-    solve();
+        for (int x = 1; x <= m; x++) {
+            f[x][y][map[x][y]] = min(f[x - 1][y][!map[x][y]], f[x][y - 1][!map[x][y]]) + 1;
+            
+            if (f[x][y][map[x][y]] > 1 && map[x - f[x][y][map[x][y]] + 1][y - f[x][y][map[x][y]] + 1] != map[x][y])
+                f[x][y][map[x][y]]--;
+            
+            if (f[x][y][map[x][y]] > ans)
+                ans = f[x][y][map[x][y]];
+        }
     
     cout << ans;
     
     return 0;
 }
-
