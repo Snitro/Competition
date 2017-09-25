b47d54fcfcb38e1bed0b02c885f05f920b435e74
物是人非 <1161052526@qq.com>
2017-09-25 20:01:16 +0800

    SCHOOL 2017 9 25 空间压缩 LCIS

--- VijosTopics/P1264 神秘的咒语.cpp
+++ VijosTopics/P1264 神秘的咒语.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 int n;
 
-int a_len, a[505], pos[505], b_len, b[505];
+int a_len, a[505], b_len, b[505];
 
-int f[505][505];
+int f[505];
 
 int main() {
 	freopen("in.txt", "r", stdin);
@@ -20,10 +20,8 @@ int main() {
 		
 		scanf("%d", &a_len);
 		
-		for (int i = 1; i <= a_len; i++) {
+		for (int i = 1; i <= a_len; i++)
 			scanf("%d", &a[i]);
-			pos[i] = i;
-		}
 		
 		scanf("%d", &b_len);
 		
@@ -31,23 +29,21 @@ int main() {
 			scanf("%d", &b[i]);
 		
 		int ans = 0;	
-		
-		for (int i = 1; i <= a_len; i++) {
-			int MAX = 0;
-			 
-			for (int j = 1; j <= b_len; j++) {
-				if (a[i] > b[j])
-					MAX = max(MAX, f[i - 1][j]);
 				
-				if (a[i] == b[j])
-					f[i][j] = MAX + 1;
-				else
-					f[i][j] = f[i - 1][j];
-			}
+		for (int i = 1; i <= a_len; i++) {
+		    int MAX = 0;
+		
+		    for (int j = 1; j <= b_len; j++) {
+		        if (a[i] > b[j])
+		            MAX = max(MAX, f[j]);
+		
+		        if (a[i] == b[j])
+		            f[j] = MAX + 1;
+		    }
 		}
 		
 		for (int i = 1; i <= b_len; i++)
-			ans = max(ans, f[a_len][i]);
+			ans = max(ans, f[i]);
 		
 		printf("%d\n", ans);
 	}
