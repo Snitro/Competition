83665484b228cf1ac399af749bfcf3e48616ac94
物是人非 <1161052526@qq.com>
2017-11-07 09:22:28 +0800

    Update endWith.cpp

--- Template/endWith.cpp
+++ Template/endWith.cpp
@@ -2,7 +2,7 @@ bool endWith(string n, string m){
     if(n.size() < m.size())
         return false;
     else{
-        for(int i = 1; i <= m.size(); i++)
+        for(int i = 1; i <= (int)m.size(); i++)
             if(n[n.size() - i] != m[m.size() - i])
                 return false;
         return true;
